7f50daad388ed32bf82dbb2b8908529d894b2d73
Mavrick91 <clownclowed2@gmail.com>
2019-06-22 03:11:38 +0900

    Setup redux

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle, { theme } from 'app/style'
 import Routes from 'app/routes'
+import { Provider } from 'react-redux'
+import store from 'app/store'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <>
-        <GlobalStyle />
-        <Routes />
-      </>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <Routes />
+        </>
+      </ThemeProvider>
+    </Provider>
   )
 }
 
