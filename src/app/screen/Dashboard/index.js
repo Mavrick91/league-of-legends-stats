a994d04b53e989c7cae87f47a1d0354199e24b88
Mavrick91 <clownclowed2@gmail.com>
2019-07-05 00:31:06 +0900

    Remove LoaderState

--- src/app/screen/Dashboard/index.js
+++ src/app/screen/Dashboard/index.js
@@ -1,21 +1,11 @@
 import { connect } from 'react-redux'
-import { FETCH_SUMMONER_REQUEST } from 'app/service/summoner/reducer'
-import { isLoaded } from 'app/service/loader/selector'
 import { getSummonerSelector } from 'app/service/summoner/selector'
 import { getSummonerId } from 'app/service/summoner/action'
 import Dashboard from './Dashboard'
 
 function mapStateToProps(state) {
-  const obj = {}
-  const isSummonerLoaded = isLoaded(state, FETCH_SUMMONER_REQUEST)
-
-  if (isSummonerLoaded) {
-    obj.summoner = getSummonerSelector(state)
-  }
-
   return {
-    isSummonerLoaded,
-    ...obj,
+    summoner: getSummonerSelector(state),
   }
 }
 
